3a07a8884b65b79586cd11dcabb74d37d66d049a
alfaceverde44 <helena.bauer@escola.pr.gov.br>
2026-08-18 00:48:16 +0000

    Cria a conexão com o MySQL

---
+++ database/backend/src/config/bd.js
@@ -0,0 +1,13 @@
+const mysql = require(&#39;mysql2/promise&#39;);
+require(&#39;dotenv&#39;).config();
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+module.exports = pool;
